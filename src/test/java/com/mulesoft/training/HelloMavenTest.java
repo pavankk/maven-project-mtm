4036c26c7d53f5d8c780ee5af8796472a957b2b6
Pavan Kumar Kolla <pkolla@guidewire.com>
2016-09-13 15:51:10 -0700

    mule-module-json added to maven dependency

--- src/test/java/com/mulesoft/training/HelloMavenTest.java
+++ src/test/java/com/mulesoft/training/HelloMavenTest.java
@@ -17,15 +17,15 @@ public class HelloMavenTest extends FunctionalTestCase {
     // retrieveFlights response contains content-type header
     // content-type headers has a value of application/json
     @Test
-   /* public void retrieveFlightsAddsAppropriateHeader() throws Exception{
+    public void retrieveFlightsAddsAppropriateHeader() throws Exception{
       MuleEvent event = runFlow("retrieveFlights");
       String contentType = event.getMessage().getOutboundProperty("Content-Type");
       assertEquals("application/json", contentType);
-    }*/
+    }
 
     // run maven-config.xml when testing
     @Override
-    protected String getConfigFile(){
+    public String getConfigFile(){
       return "maven-project.xml";
     }
 
